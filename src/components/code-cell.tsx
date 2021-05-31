0dc7d4a684afb9da8c7aff336fdf2b312eadd75c
b.fabry <b.fabry@groupeonepoint.com>
2021-05-31 16:09:31 +0200

    Bundle Start and Bundle Complete

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./Preview";
-import bundle from "../bundler";
 import Resizable from "./Resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
+import { useTypedSelector } from "../hooks/use-typed-selector";
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState("");
-  const [err, setErr] = useState("");
-  const { updateCell } = useActions();
+  const { updateCell, createBundle } = useActions();
 
   useEffect(() => {
+    const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    console.log(bundle);
+
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setErr(output.err);
+      createBundle(cell.id, cell.content);
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content]);
+  }, [cell.content, cell.id]);
 
   return (
     <Resizable direction="vertical">
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import cellsReducers from "./cellsReducers";
+import bundlesReducers from "./bundlesReducers";
 
 const reducers = combineReducers({
   cells: cellsReducers,
+  bundles: bundlesReducers,
 });
 
 export default reducers;
