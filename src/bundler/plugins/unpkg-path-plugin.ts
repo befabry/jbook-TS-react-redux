146b77c68587047649cee0e40d4597d517e3e3c8
b.fabry <b.fabry@groupeonepoint.com>
2021-06-24 17:15:42 +0200

    cumulative code execution

--- src/bundler/plugins/unpkg-path-plugin.ts
+++ src/bundler/plugins/unpkg-path-plugin.ts
@@ -4,21 +4,20 @@ export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      //Handle root entry file of "index.js"
+      // Handle root entry file of 'index.js'
       build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return { path: "index.js", namespace: "a" };
       });
 
-      //Handle relative paths in a module
-      build.onResolve({ filter: /(^\.+\/)/ }, (args: any) => {
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
           namespace: "a",
-          path: new URL(args.path, `https://unpkg.com/${args.resolveDir}/`)
-            .href,
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
         };
       });
 
-      //Handle main file of a module
+      // Handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: "a",
